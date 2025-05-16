7fc9eefba954acb6025523e22f78c82a9972ea89
Alain Pham <apham@work>
2025-05-16 16:37:02 +0200

    alloy with docker integration

--- utils/gcloud-dl-dashboards.sh
+++ utils/gcloud-dl-dashboards.sh
@@ -5,7 +5,7 @@ export dashboardfolder=$script_dir/../templates/grafana/config/dashboards
 source $script_dir/../init
 
 
-dashboard_ids=(
+linux_dashboard_ids=(
     node-fleet
     nodes
     node-system
@@ -15,14 +15,26 @@ dashboard_ids=(
     node-network
 )
 
+docker_dashboard_ids=(
+    integration-docker-overview
+    integration-docker-logs
+)
+
 # Linux integration
 mkdir -p "$dashboardfolder/Integration - Linux Node"
-for id in "${dashboard_ids[@]}"; do
+for id in "${linux_dashboard_ids[@]}"; do
     curl -s "https://ap.grafana.net/api/dashboards/uid/$id" \
     --header "Authorization: Bearer $GCLOUD_AP_GRAFANA_NET_BEARER_TOKEN" | jq '.dashboard' > "$dashboardfolder/Integration - Linux Node/$id.json"
+done
+
+# Docker integration
+mkdir -p "$dashboardfolder/Integration - Docker"
+for id in "${docker_dashboard_ids[@]}"; do
+    curl -s "https://ap.grafana.net/api/dashboards/uid/$id" \
+    --header "Authorization: Bearer $GCLOUD_AP_GRAFANA_NET_BEARER_TOKEN" | jq '.dashboard' > "$dashboardfolder/Integration - Docker/$id.json"
 done
 
 # OpenTelemetry services
 
 mkdir -p "$dashboardfolder/Integration - Application Services/"
-curl -o "$dashboardfolder/Integration - Application Services/opentelemetry-for-http-services.json" -s https://raw.githubusercontent.com/alainpham/app-archetypes/refs/heads/master/observability/dashboards/opentelemetry-for-http-services.json
+curl -o "$dashboardfolder/Integration - Application Services/opentelemetry-for-http-services.json" -s https://raw.githubusercontent.com/alainpham/app-archetypes/refs/heads/master/observability/dashboards/Integration%20-%20Application%20Services/opentelemetry-for-http-services.json
