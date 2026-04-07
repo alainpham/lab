7506b4555d286cba08a594a17882c7eccd4d49e5
alainpham <13981031+alainpham@users.noreply.github.com>
2026-04-07 23:23:21 +0200

    peer get

--- templates/wgserver/config/mapping.sh
+++ templates/wgserver/config/mapping.sh
@@ -1,25 +1,39 @@
-    peersmapping=(
-        # servers
-        "p2|aaon"
-        "p3|popi"
-        # pcs
-        "p20|lpro"
-        "p21|lg15"
-        "p22|mbm1"
-        "p23|idea"
-        "p24|fair"
-        "p25|hpkw"
-        # phones & tablets
-        "p40|iphone 8s quang"
-        "p41|ipad air quang"
-        "p42|ipad a16 quang"
-        "p43|thomson quang"
-        "p44|ipad 9 max"
-        "p45|iphone 14 pro max hoa"
-    )
+peersmapping=(
+    # servers
+    "aaon|p2"
+    "popi|p3"
+    # pcs
+    "lpro|p20"
+    "lg15|p21"
+    "mbm1|p22"
+    "idea|p23"
+    "fair|p24"
+    "hpkw|p25"
+    # phones & tablets
+    "iphone-8s-quang|p40"
+    "ipad-air-quang|p41"
+    "ipad-a16-quang|p42"
+    "thomson-quang|p43"
+    "ipad-9-max|p44"
+    "iphone-14-pro-max-hoa|p45"
+)
 
+get_peer_value() {
+    local key="$1"
     for entry in "${peersmapping[@]}"; do
         peernb="${entry%%|*}"
         name="${entry#*|}"
-    echo $peernb "->" $name
+        if [[ "$peernb" == "$key" ]]; then
+            echo "$name"
+            return 0
+        fi
     done
+    echo "Key not found"
+    return 1
+}
+
+# Usage: get_peer_value "aaon"
+if [[ -n "$1" ]]; then
+    get_peer_value "$1"
+fi
+
